3c368f5b49a78a6a656ac51497a391d0ab92630b
Weslley Alves <wesshead@gmail.com>
2012-01-20 21:34:36 -0200

    Adicionado o comando "setValue" para setar dados no elemento

--- jQuery/jquery.formjson.js
+++ jQuery/jquery.formjson.js
@@ -4,8 +4,8 @@
      * via parâmetro.
      * @author Weslley Alves
      * @email wesshead@gmail.com
-     * @version v0.0.1
-     * @date 2011-12-01
+     * @version v0.0.2
+     * @date 2011-12-19
      * @param {Object} data Obejto com os dados para os campos do Formulário
      * @return {Wrapped} Retorna o Elemento do Formulário
      * @use O atributo do Object ("nome") seja igual ao valor do atributo "name" do campo.
@@ -13,40 +13,54 @@
      * HTML: <input id="nome" name="nome" type="text" />
      * Chamada: $('form').loadData({'nome':'Weslley Alves'});
      */
-    $.fn.loadData = function(data){         
+    $.fn.loadData = function(data){
         // Interando sobre o Object passado e setando os valores nos campos
         for(var campo in data){
-            // Recuperando o campo
+            // Recuperando o elemento do campo
             var el = $(this).find('[name="'+ campo +'"]');
             
             // Verificando se foi resgatado mais de um Objeto
             if(el.length > 1){
                 // LOOP para verificar se deve ser setado o valor
                 $(el).each(function(i,v){
-                    // Campo "checkbox" ou "radio"
-                    if($(this).is('input[type=checkbox]') || $(this).is('input[type=radio]')){
-                        // Verificando se o valor do elemento é igual ao passado no Object
-                        if($(this).is('input[value='+ data[campo] +']')){
-                            $(this).attr('checked', true);
-                        }
-                    } else { // Demais campos
-                        $(this).val(data[campo]);
-                    }
+                    // Setando o valor no Elemento
+                    $(this).setValue(data[campo]);
                 });
             } else{
-                // Campo "checkbox" ou "radio"
-                if($(el).is('input[type=checkbox]') || $(el).is('input[type=radio]')){
-                    // Verificando se o valor do elemento é igual ao passado no Object
-                    if($(el).is('input[value='+ data[campo] +']')){
-                        $(el).attr('checked', true);
-                    }
-                } else { // Demais campos
-                    $(el).val(data[campo]);
-                }
+                // Setando o valor no Elemento
+                $(el).setValue(data[campo]);
             }
         } // Fim da interação no Objeto com os dados
         
         // Retornando o Elemento do Formulário
+        return this;
+    }
+
+    /**
+     * Método que irá setar o valor passado via parâmetro no Elemento referênciado
+     * @author Weslley Alves
+     * @email wesshead@gmail.com
+     * @version v0.0.1
+     * @date 2011-12-19
+     * @param {Int/String} value Valor a ser setado no Elemento referênciado
+     * @return {Element} Retorna o Elemento referênciado
+     * @use Deve ser chamado este método através do Elemento HTML.
+     * @example
+     * HTML: <input id="nome" name="nome" type="text" />
+     * Chamada: $('#nome').setValue('Weslley Alves');
+     */
+    $.fn.setValue = function(value){
+        // Campo "checkbox" ou "radio"
+        if($(this).is('input[type=checkbox]') || $(this).is('input[type=radio]')){
+            // Verificando se o valor do elemento é igual ao passado no Object
+            if($(this).is('input[value='+ value +']')){
+                $(this).attr('checked', true);
+            }
+        } else { // Demais campos
+            $(this).val(value);
+        }
+
+        // Retornando o Elemento referênciado
         return this;
     }
 })(jQuery);
